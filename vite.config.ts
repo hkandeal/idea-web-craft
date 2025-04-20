78ef0b3373800e6c1865beb88928fa0056b27748
Hossam Kandel <hossam20052@hotmail.com>
2025-04-20 22:57:08 +0400

    fix allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  preview: {
+    allowedHosts: ['hossam.io']
+  },
+  
   server: {
     host: "::",
     port: 8080,
